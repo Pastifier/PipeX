4ac4dde37d4a960d6bc14f8b0e232b27fe6de1ae
Emran Binjamaan <therealemran.design@gmail.com>
2024-01-19 22:29:43 +0400

    Notes

--- srcs/mandatory/main.c
+++ srcs/mandatory/main.c
@@ -6,19 +6,25 @@
 /*   By: ebinjama <ebinjama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 16:00:07 by ebinjama          #+#    #+#             */
-/*   Updated: 2024/01/19 16:57:44 by ebinjama         ###   ########.fr       */
+/*   Updated: 2024/01/19 22:23:14 by ebinjama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 #include "log.h"
 
-int	main(int c, char *v[], char *envp[])
-{
-	t_paths	paths;
-	int		access_error;
+static bool commit_fork(char *v[], char *paths[]);
 
-	(void) v;
+// TODO:
+// Parse arguments that contain flags.
+// Turn them into a 2D array that execve can accept.
+int main(int c, char *v[], char *envp[])
+{
+	t_paths paths;
+	int access_error;
+	pid_t child;
+
+	(void)v;
 	if (c != 5)
 		return (wexit(BAD_FORMAT), 1);
 	paths = init_paths(envp, 5);
@@ -27,6 +33,6 @@ int	main(int c, char *v[], char *envp[])
 	access_error = access(v[1], R_OK);
 	if (access_error)
 		wstrerror(v[1], ENOENT);
-	// bois to free: {paths.arr}
+	child = fork();
 	return (free_paths(&paths), 0);
 }
